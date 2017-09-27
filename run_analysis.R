0c97b48df740669eb31483fd0cc3bbd24800e4fb
Laura Ochoa <laura.ochoagomez@swisscom.com>
2017-09-27 09:19:58 +0200

    took only mean and std

--- run_analysis.R
+++ run_analysis.R
@@ -7,36 +7,82 @@
 # called CodeBook.md.
 # 4) a README.md that explains how all scripts work together
 
+# Download data and save file. Couldn't unzip automatically so did manually
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/dataset.zip")
-# unzip somehow (did it manually)
+# unzip somehow 
 
-# 1. Merge training and test sets to create one dataset
+# 1. Merge training and test sets to create one dataset (DONE)
 # 'train/X_train.txt': Training set. and 'test/X_test.txt': Test set.
 testSet = read.table("./data/UCI HAR Dataset/test/X_test.txt")
 names(testSet)
 trainSet = read.table("./data/UCI HAR Dataset/train/X_train.txt")
 names(trainSet)
 
-# merges both sets of data into one
+# merges both sets of data into one 
 mergedData = rbind(testSet,trainSet)
+# checking it went ok
+dim(testSet)
+dim(trainSet)
+dim(mergedData)
+
+testSet[1:3]
+trainSet[1:3]
+mergedData[1:3]
+class(mergedData[1])
 
 # 2. Extracts only the measurements on the mean and standard 
 #    deviation for each measurement.
-dim(testSet)
-testSet[1:3]
-trainSet[1:3]
-dim(trainSet)
-head(mergedData)
-dim(mergedData)
-class(mergedData[1])
+# Each row is a 561-feature vector with time and frequency domain variables.
+
+#load tidyr to clean data
+library(tidyr,dplyr)
+
+#add features_info into the dataset so that we can filter it
+features = read.table("./data/UCI HAR Dataset/features.txt")
+head(features)
+features2 <- filter(features, grepl("-mean()|-std()",V2))
+features3 <- filter(features2, grepl("meanFreq()",V2), invert)
+features4 <- anti_join(features2,features3)
+#according to features4, we need to clean mergedData columns
+features4 <- arrange(features4,V1)
+
+for (i in 1:ncol(mergedData)) {
+  newname <- toString(features$V2[i])
+  oldname <- names(mergedData[i])
+  #rename(mergedData, newname = oldname) #always gave error
+  names(mergedData)[names(mergedData) == oldname] <- newname
+}
+
+select(mergedData,-contains("bandsEnergy"))
+
+#remove unwanted columns
+mergedDataA <- mergedData[1:460]
+mergedDataB <- mergedData[503:ncol(mergedData)]
+mergedDataC <- cbind(mergedDataA,mergedDataB)
+
+mergedData %>%
+  select(contains("mean") OR contains("std")) 
+#%>%
+#  gather(part_sex, count, -score_range) %>%
+#  separate(part_sex, c("part", "sex")) %>%
+#  group_by(part,sex) %>%
+#  mutate(total = sum(count),
+#         prop = count/total
+#  ) %>% print
+
+
+
 # get mean and standard deviations
 sapply(mergedData,mean)
 sapply(mergedData,sd)
+
 # 3. Uses descriptive activity names to name the activities 
 #    in the data set
+
 # 4. Appropriately labels the data set with descriptive variable names.
+
 # 5. From the data set in step 4, creates a second, independent 
 #    tidy data set with the average of each variable for each 
 #    activity and each subject.
